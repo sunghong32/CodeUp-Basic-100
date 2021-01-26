95bdf2d62dc1047ff8f1a7245d489de5b195afcd
민성홍 <sunghong32@naver.com>
2021-01-27 00:18:56 +0900

    1019번

---
+++ 1019.py
@@ -0,0 +1,13 @@
+# y,m,d = input().split('.')
+# print('%04d' % int(y), end='.')
+# print('%02d' % int(m), end='.')
+# print('%02d' % int(d))
+
+a,b,c=input().split('.') 
+print('%04d' % int(a), end='.') 
+print('%02d' % int(b), end='.') 
+print('%02d' % int(c))
+
+# y, m, d = map(int, input().split('.'))
+# print('%d.%02d.%02d' % (y,m,d))
+
